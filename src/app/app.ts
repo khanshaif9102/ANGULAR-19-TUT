cf2c59ce19b280960afdbc64916c79cace2f85f1
khanshaif9102 <khanshaif9102@gmail.com>
2026-07-04 15:25:03 +0530

    custom components

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from "./login/login";
 import { Signup } from './signup/signup';
+import { ProfileComponent } from "./profile/profile";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, Signup],
+  imports: [RouterOutlet, Login, Signup, ProfileComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,14 @@
+
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'profile',
+    imports: [],
+    templateUrl: './profile.html',
+    styleUrl: './profile.css'
+})
+
+export class ProfileComponent {
+
+}
