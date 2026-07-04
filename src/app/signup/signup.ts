243bf25abf4701a3cc56f6892b879d52dab4bff6
khanshaif9102 <khanshaif9102@gmail.com>
2026-07-04 15:15:58 +0530

    first commit of learning angular

---
+++ src/app/signup/signup.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'signup',
+  imports: [],
+  templateUrl: './signup.html',
+  styleUrl: './signup.css',
+})
+export class Signup { }
